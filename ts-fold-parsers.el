8a694b7148cc6231c673e0cfd1472818762d4765
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-03-31 20:02:01 +0800

    chore: add if statement support for ts-fold-parsers-python.
    Thanks to https://emacs-china.org/t/emacs-vscdoe-python/24582/17.

--- ts-fold-parsers.el
+++ ts-fold-parsers.el
@@ -569,6 +569,7 @@
     (class_definition         . ts-fold-range-python-def)
     (list                     . ts-fold-range-seq)
     (dictionary               . ts-fold-range-seq)
+    (if_statement             . ts-fold-range-seq)
     (expression_statement     . ts-fold-range-python-expression-statement)
     (comment
      . (lambda (node offset)
